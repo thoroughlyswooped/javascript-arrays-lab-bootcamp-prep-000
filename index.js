2ecee33aa3d2b6004144a26c1e6685e1131db975
Ezra T Huffman <joshooha@gmail.com>
2018-09-16 19:05:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destrucivelyRemoveLastKiten() {
+  kittens.pop();
+}
+
